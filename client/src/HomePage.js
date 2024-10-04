91f032ee051b7741bb59cca65eca51cf42c62e3b
Kade Salerno <113953505+ksal44@users.noreply.github.com>
2024-10-04 16:41:25 -0500

    minor language updates

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Insurance Package Builder</h1>
+      <h1>Welcome to the DCBG Agent Portal</h1>
       <nav>
         <ul>
           <li><Link to="/signin">Sign In</Link></li>
-          <li><Link to="/packages">View Packages</Link></li>
+          <li><Link to="/packages">Package Builder</Link></li>
           <li><Link to="/company-tracker">Company Tracker</Link></li>
         </ul>
       </nav>
